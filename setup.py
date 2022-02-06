741ec6e1d2d929d29c7c5ccb2d98442706f75bf2
Michael Lynch <mtlynch@users.noreply.github.com>
2022-02-05 20:11:00 -0500

    Add PyPI configuration (#4)
    * Add PyPI configuration
    
    * Remove e2e dependency

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os.path
+
+import setuptools
+
+setuptools.setup(
+    name='zestful-parse-ingredient',
+    long_description=open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                     'README.md')).read(),
+    long_description_content_type="text/markdown",
+    version='0.1.0',
+    description='Import Mercury banking transactions into beancount format',
+    author='Michael Lynch',
+    license="MIT",
+    keywords="mercury beancount bookkeeping finance",
+    url='https://github.com/mtlynch/beancount-mercury.git',
+    packages=['beancount_mercury'],
+    install_requires=[],
+    python_requires='>=3.7',
+)
