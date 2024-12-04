9fadc263e5ad68b7c8bee2b8670af97a4de7742a
Michael Lynch <git@mtlynch.io>
2024-12-04 16:39:56 -0500

    Only need one case

--- beancount_chase/checking.py
+++ beancount_chase/checking.py
@@ -128,7 +128,7 @@ _OUTBOUND_TRANSFER_PATTERN = re.compile(
     r'Online Transfer \d+ to (.+?)\s*transaction #', re.IGNORECASE)
 
 _ACH_PAYMENT_PATTERN = re.compile(
-    r'^[A-Za-z-]+ ACH Payment \d+ [Tt]o ([A-Za-z]+) \(_#+\d+\)$', re.IGNORECASE)
+    r'^[a-z-]+ ACH Payment \d+ To ([a-z]+) \(_#+\d+\)$', re.IGNORECASE)
 
 _INBOUND_TRANSFER_PATTERN = re.compile(
     r'Online Transfer \d+ from (.+?)\s*transaction #', re.IGNORECASE)
